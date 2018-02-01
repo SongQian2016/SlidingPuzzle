3b577cd96ad11a79f96d482947093c948505eb64
Qian <qsongq@gmail.com>
2018-02-01 07:48:52 -0800

    Menu options modification.

--- app/src/main/java/groupapp/cs/psu/slidingpuzzle/MathModeSelectActivity.java
+++ app/src/main/java/groupapp/cs/psu/slidingpuzzle/MathModeSelectActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.TextView;
@@ -25,7 +26,7 @@ public class MathModeSelectActivity extends AppCompatActivity {
 
     }
 
-    @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
 
         MenuInflater menuInflater = getMenuInflater();
@@ -34,4 +35,28 @@ public class MathModeSelectActivity extends AppCompatActivity {
         return true;
     }
 
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+         switch (item.getItemId()) {
+             case R.id.mathItem1:
+                 Intent intent3 = new Intent(MathModeSelectActivity.this, Math1playerActivity.class);
+                 this.startActivity(intent3);
+                 break;
+
+             case R.id.mathItem2:
+                 Intent intent4 = new Intent(MathModeSelectActivity.this, Math2playersActivity.class);
+                 this.startActivity(intent4);
+                 break;
+
+             case R.id.mathItem3:
+                 Intent intent5 = new Intent(MathModeSelectActivity.this, MathCutthroughActivity.class);
+                 this.startActivity(intent5);
+                 break;
+             default:
+                 return super.onOptionsItemSelected(item);
+         }
+         return true;
+
+    }
 }
--- app/src/main/java/groupapp/cs/psu/slidingpuzzle/NunModeSelectActivity.java
+++ app/src/main/java/groupapp/cs/psu/slidingpuzzle/NunModeSelectActivity.java
@@ -1,9 +1,11 @@
 package groupapp.cs.psu.slidingpuzzle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class NunModeSelectActivity extends AppCompatActivity {
 
@@ -11,14 +13,39 @@ public class NunModeSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nun_mode_select);
+
     }
 
-
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        MenuInflater menuInflater = getMenuInflater();
+            MenuInflater menuInflater = getMenuInflater();
 
-        menuInflater.inflate(R.menu.menu_options, menu);
-        return true;
+            menuInflater.inflate(R.menu.menu_options, menu);
+            return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.item1:
+                    Intent intent1 = new Intent(NunModeSelectActivity.this, NumberGameActivity.class);
+                    startActivity(intent1);
+                    break;
+
+                case R.id.item2:
+                    Intent intent2 = new Intent(NunModeSelectActivity.this, NumberGameAIActivity.class);
+                    startActivity(intent2);
+                    break;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+
+            }
+            return true;
+    }
+
 }
+
+
