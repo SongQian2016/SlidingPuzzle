72fe23d003e526334f96fd3990c68a4b0f3270e4
srilakshmishivakumar <srilakshmi.shk@gmail.com>
2018-03-21 14:58:55 -0700

    Mode Selection

---
+++ app/src/main/java/groupapp/cs/psu/slidingpuzzle/ModeSelectActivity.java
@@ -0,0 +1,47 @@
+package groupapp.cs.psu.slidingpuzzle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+public class ModeSelectActivity  extends AppCompatActivity{
+
+    public Button numBtn, mathBtn, backBtn;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modeselection);
+
+        numBtn = (Button) findViewById(R.id.numButton);
+
+        mathBtn = (Button) findViewById(R.id.mathButton);
+
+        mathBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNum1();
+            }
+            private void openNum1() {
+
+                Intent intent = new Intent(ModeSelectActivity.this, MathModeLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        numBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNum();
+            }
+            private void openNum() {
+                Intent intent = new Intent(ModeSelectActivity.this, NunModeSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+
+
